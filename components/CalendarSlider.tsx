dfdb6d7783f222bc19c060b4c5b48813659ff9e8
Bobby Connolly <bobbyconnolly@gmail.com>
2025-02-13 12:22:28 -0500

    Rescale sliders

--- components/CalendarSlider.tsx
+++ components/CalendarSlider.tsx
@@ -32,12 +32,24 @@ const CalendarSlider = () => {
   const aspectRatio = chassisHeight / chassisWidth;
   const maxDragDistance = containerWidth * 0.5; // 50% of container width
 
+  interface SliderConfig {
+    type: "weekday" | "month" | "day";
+    width: number;
+    height: number;
+    y: number;
+    displayHeight: number;
+  }
+
+  const sliderConfigs: SliderConfig[] = [
+    { type: "weekday", width: 3692, height: 200, y: 145, displayHeight: 40 },
+    { type: "month", width: 3692, height: 194, y: 213, displayHeight: 39 },
+    { type: "day", width: 3698, height: 190, y: 284, displayHeight: 38 },
+  ];
+
   // Calculate relative positions
-  const sliderPositions = {
-    weekday: { y: `${(581 / chassisHeight) * 25}%` },
-    month: { y: `${(859 / chassisHeight) * 25}%` },
-    day: { y: `${(1137 / chassisHeight) * 25}%` },
-  };
+  const sliderPositions = Object.fromEntries(
+    sliderConfigs.map((config) => [config.type, { y: `${(config.y / chassisHeight) * 100}%` }])
+  );
 
   const handleMouseDown = (e: React.MouseEvent, type: string) => {
     setIsDragging(type);
@@ -96,28 +108,32 @@ const CalendarSlider = () => {
       </div>
 
       {/* Sliders */}
-      {["weekday", "month", "day"].map((type, index) => (
-        <div
-          key={type}
-          className="absolute left-1/2 cursor-grab active:cursor-grabbing"
-          style={{
-            top: sliderPositions[type as keyof typeof sliderPositions].y,
-            width: "650px",
-            transform: `translateX(calc(-50% + ${sliderOffsets[type as keyof typeof sliderOffsets]}px))`,
-            userSelect: "none",
-          }}
-          onMouseDown={(e) => handleMouseDown(e, type)}
-        >
-          <Image
-            src={`/images/slider${index + 1}.png`}
-            alt={`${type} slider`}
-            width={3692}
-            height={200}
-            className="w-full h-auto"
-            draggable={false}
-          />
-        </div>
-      ))}
+      {["weekday", "month", "day"].map((type, index) => {
+        const config = sliderConfigs[index];
+        return (
+          <div
+            key={type}
+            className="absolute left-1/2 cursor-grab active:cursor-grabbing"
+            style={{
+              top: sliderPositions[type as keyof typeof sliderPositions].y,
+              width: "750px",
+              height: `${config.displayHeight}px`,
+              transform: `translateX(calc(-50% + ${sliderOffsets[type as keyof typeof sliderOffsets]}px))`,
+              userSelect: "none",
+            }}
+            onMouseDown={(e) => handleMouseDown(e, type)}
+          >
+            <Image
+              src={`/images/slider${index + 1}.png`}
+              alt={`${config.type} slider`}
+              width={config.width}
+              height={config.height}
+              className="w-full h-full object-cover"
+              draggable={false}
+            />
+          </div>
+        );
+      })}
 
       {/* Diamonds overlay */}
       <div className="absolute left-1/2" style={{ width: "77px", transform: "translateX(-50%)" }}>
